24151e5c68ce1be5bc50bf3006ad3266fcb38910
wenqiang.zheng <wenqiang.zheng@jdxiaokang.cn>
2020-03-05 12:04:56 +0800

    调整

--- commons-utils/src/main/java/com/jdxiaokang/commons/utils/FileUtils.java
+++ commons-utils/src/main/java/com/jdxiaokang/commons/utils/FileUtils.java
@@ -1,6 +1,10 @@
 package com.jdxiaokang.commons.utils;
 
+import sun.misc.BASE64Encoder;
+
 import java.io.IOException;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 
 /**
  * @author zwq  wenqiang.zheng@jdxiaokang.cn
@@ -14,13 +18,13 @@ public class FileUtils {
      * filename 下载文件名 agent 客户端浏览器
      */
     public static String encodeDownloadFilename(String filename, String agent) throws IOException {
-//        if (agent.contains("Firefox")) { // 火狐浏览器
-//            filename = "=?UTF-8?B?" + new BASE64Encoder().encode(filename.getBytes(StandardCharsets.UTF_8)) + "?=";
-//            filename = filename.replaceAll("\r\n", "");
-//        } else { // IE及其他浏览器
-//            filename = URLEncoder.encode(filename, "utf-8");
-//            filename = filename.replace("+", " ");
-//        }
+        if (agent.contains("Firefox")) { // 火狐浏览器
+            filename = "=?UTF-8?B?" + new BASE64Encoder().encode(filename.getBytes(StandardCharsets.UTF_8)) + "?=";
+            filename = filename.replaceAll("\r\n", "");
+        } else { // IE及其他浏览器
+            filename = URLEncoder.encode(filename, "utf-8");
+            filename = filename.replace("+", " ");
+        }
         return filename;
     }
 }
